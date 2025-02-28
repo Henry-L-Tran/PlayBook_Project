924cd6425df6084292f64740d56944197186b02d
Henry-L-Tran <henrytran1129@gmail.com>
2025-02-27 23:47:13 -0600

    Created the base homepage for the UI and its route after logging in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Register from './Register.jsx'
 import Login from './Login.jsx'
+import Home from './Home.jsx'
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+
+function Home() {
+
+
+    return (
+        <div>
+            <h1>Playbook</h1>
+        </div>
+    )
+}
+
+export default Home
