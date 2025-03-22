e4bf059b66ec98863145ca7aec2268000f2e95b8
Henry-L-Tran <henrytran1129@gmail.com>
2025-03-22 02:45:09 -0500

    Implemented functional tabs that switches between different betting categories (NBA, NFL, LoL, VAL) with small designs

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Tabs, Tab } from "@mui/material";
 import { Box, Typography } from "@mui/material";
 
 function Home() {
 
   const [nbaLiveGames, setNbaLiveGames] = useState({gameDate: "", gameData: []});
+  const [activeCategoryTab, setActiveCategoryTab] = useState("NBA");
 
   useEffect(() => {
     const fetchNbaLiveGames = async () => {
@@ -42,6 +44,10 @@ function Home() {
     return `${minutes}:${seconds}`;
   }
 
+  const handleCategoryTabChange = (event, newValue) => {
+    setActiveCategoryTab(newValue);
+  }
+
 
   return (
     
@@ -63,180 +69,260 @@ function Home() {
           backgroundColor: "rgba(0, 0, 0, 0.3)",
           borderRadius: "rem",
         }}>
-        <Typography variant="h4" className="font-mono mb-4">
-          NBA Live Scores
-        </Typography>
+
+        <Tabs
+          value={activeCategoryTab}
+          onChange={handleCategoryTabChange}
+          centered
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "white",
+              height: "0.25rem",
+              borderRadius: "1rem",
+            },
+          }}
+          sx={{
+            marginTop: "1rem",
+            textDecorationColor: "white",
+          }}
+        >
+          <Tab label="NBA" value="NBA"
+            disableRipple
+            sx={{
+              mx: 5,
+              fontSize: "1.5rem",
+              fontFamily: "monospace",
+              textColor: "inherit",
+              color: "white",
+              outline: "none",
+              "&:focus": {
+                outline: "none",
+                color: "white",
+                fontWeight: "bold",
+              },
+            }} />
+          <Tab label="NFL" value="NFL"
+            disableRipple
+            sx={{
+              mx: 5,
+              fontSize: "1.5rem",
+              fontFamily: "monospace",
+              textColor: "inherit",
+              color: "white",
+              "&:focus": {
+                outline: "none",
+                color: "white",
+                fontWeight: "bold",
+              },
+            }} />
+          <Tab label="LoL" value="LoL"
+            disableRipple
+            sx={{
+              textTransform: "none",
+              mx: 5,
+              fontSize: "1.5rem",
+              fontFamily: "monospace",
+              textColor: "inherit",
+              color: "white",
+              "&:focus": {
+                outline: "none",
+                color: "white",
+                fontWeight: "bold",
+              },
+            }} />
+          <Tab label="VAL" value="VAL" 
+            disableRipple
+            sx={{
+              mx: 5,
+              fontSize: "1.5rem",
+              fontFamily: "monospace",
+              textColor: "inherit",
+              color: "white",
+              "&:focus": {
+                outline: "none",
+                color: "white",
+                fontWeight: "bold",
+              },
+            }} />
+        </Tabs>
         
-        <Typography
-                  sx={{
-                    fontSize: "1.2rem",
-                    fontFamily: "monospace",
-                    width: "20rem",
-                    paddingTop: "1rem",
-                    marginLeft: "26rem",
-                  }}> {nbaLiveGames.gameDate} 
-          </Typography>
-
-        {nbaLiveGames.length === 0 ? (
-          <Typography> No scheduled games </Typography>
-        ) : (
-          nbaLiveGames.gameData.map((game, index) => (
-            <Box key={index}
+        {activeCategoryTab === "NBA" && (
+          <>
+            <Typography
               sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                gap: "2rem",
-                padding: "2rem",
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                borderRadius: "0.5rem",
-                marginBottom: "1rem",
-                alignItems: "flex-start",
-                textAlign: "left",
-                paddingLeft: "10rem",
-                gridTemplateColumns: "80px 80px 200px 80px",
-              }}>
-              <Box
-                sx={{
-                  display: "grid",
-                  gridTemplateColumns: "80px 80px 240px 1fr",
-                  alignItems: "center",
-                }}
-                >
-                <Typography variant="h6"
+                fontSize: "1.2rem",
+                fontFamily: "monospace",
+                width: "20rem",
+                paddingTop: "1rem",
+                marginLeft: "26rem",
+              }}> {nbaLiveGames.gameDate} 
+              </Typography>
+
+            {nbaLiveGames.length === 0 ? (
+              <Typography> No scheduled games </Typography>
+            ) : (
+              nbaLiveGames.gameData.map((game, index) => (
+                <Box key={index}
                   sx={{
-                    color: game.gameStatus === 3 && game.awayTeam.score > game.homeTeam.score ? "#10833C" : "white",
-                  }}> {game.awayTeam.teamTriCode} </Typography>
-                <Typography 
-                  sx={{
-                    fontWeight: "bold",
-                    fontSize: "0.75rem",
-                  }}> {game.awayTeam.wins} - {game.awayTeam.losses} </Typography>
-
-                <Box className="mt-2">
-                  <Box className="grid grid-cols-5 text-white gap-x-8 ml-40">
-                    {game.awayTeam.periods.map((period, index) => (
-                      <Box key={index} className="text-center">
-                        <Typography 
-                          sx={{
-                            fontFamily: "monospace",
-                            gridTemplateColumns: "repeat(4, 40px) 60px",
-                          }}
-                            className="text-center"
-                          >
-                          {period.period}
-                        </Typography>
-
-                        <Typography
-                          sx={{
-                            fontFamily: "monospace",
-                            fontSize: "0.75rem",
-                          }}
-                          className="text-center"
-                          >
-                          {period.score}
-                        </Typography>
-                      </Box>                          
-                    ))}
-                    <Typography
-                      sx={{
-                        fontFamily: "monospace",
-                        fontSize: "1.3rem",
-                        color: game.gameStatus === 3 && game.awayTeam.score > game.homeTeam.score ? "#10833C" : "white",
-                      }}> {game.awayTeam.score} </Typography>
-                  </Box>
-                </Box>
-              </Box>
-
-              <Box
-                sx={{
-                  display: "grid",
-                  gridTemplateColumns: "80px 80px 240px 1fr",
-                  alignItems: "center",
-                }}
-              >
-                <Typography variant="h6"
-                  sx={{
-                    color: game.gameStatus === 3 && game.homeTeam.score > game.awayTeam.score ? "#10833C" : "white",
-                  }}> {game.homeTeam.teamTriCode} </Typography>
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    fontSize: "0.75rem",
-                  }}> {game.homeTeam.wins} - {game.homeTeam.losses} </Typography>
-
-              <Box className="mt-2">
-                  <Box className="grid grid-cols-5 text-white gap-x-8 ml-40">
-                    {game.homeTeam.periods.map((period, index) => (
-                      <Box key={index} className="text-center">
-                        <Typography 
-                          sx={{fontFamily: "monospace"}}
-                          className="text-center"
-                          >
-                          {period.period}
-                        </Typography>
-
-                        <Typography
-                          sx={{
-                            fontFamily: "monospace",
-                            fontSize: "0.75rem",
-                          }}
-                          className="text-center"
-                          >
-                          {period.score}
-                        </Typography>
-                      </Box>                          
-                    ))}
-                    <Typography
-                      sx={{
-                        fontFamily: "monospace",
-                        fontSize: "1.3rem",
-                        color: game.gameStatus === 3 && game.homeTeam.score > game.awayTeam.score ? "#10833C" : "white",
-                      }}> {game.homeTeam.score} 
-                    </Typography>
-                  </Box>
-                </Box>
-              </Box>
-
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: "1rem",
-                  fontFamily: "monospace",
-                  marginTop: "1rem",
-                  
-                }}
-              >
-                <Typography
-                  sx={{
-                    fontSize: "1.2rem",
-                    fontWeight: "bold",
-                    fontFamily: "monospace",
-                    width: "20rem",
-                    marginLeft: "30rem",
-                    marginTop: "-12rem",
-                    textAlign: "right",
-
-                  }}> {game.gameStatusText} 
-                </Typography>
-
-                {game.gameStatus === 2 && (
-                  <Typography
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    gap: "2rem",
+                    padding: "2rem",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    borderRadius: "0.5rem",
+                    marginBottom: "1rem",
+                    alignItems: "flex-start",
+                    textAlign: "left",
+                    paddingLeft: "10rem",
+                    gridTemplateColumns: "80px 80px 200px 80px",
+                  }}>
+                  <Box
                     sx={{
-                      fontSize: "1.2rem",
+                      display: "grid",
+                      gridTemplateColumns: "80px 80px 240px 1fr",
+                      alignItems: "center",
+                    }}
+                    >
+                    <Typography variant="h6"
+                      sx={{
+                        color: game.gameStatus === 3 && game.awayTeam.score > game.homeTeam.score ? "#10833C" : "white",
+                      }}> {game.awayTeam.teamTriCode} </Typography>
+                    <Typography 
+                      sx={{
+                        fontWeight: "bold",
+                        fontSize: "0.75rem",
+                      }}> {game.awayTeam.wins} - {game.awayTeam.losses} </Typography>
+
+                    <Box className="mt-2">
+                      <Box className="grid grid-cols-5 text-white gap-x-8 ml-40">
+                        {game.awayTeam.periods.map((period, index) => (
+                          <Box key={index} className="text-center">
+                            <Typography 
+                              sx={{
+                                fontFamily: "monospace",
+                                gridTemplateColumns: "repeat(4, 40px) 60px",
+                              }}
+                                className="text-center"
+                              >
+                              {period.period}
+                            </Typography>
+
+                            <Typography
+                              sx={{
+                                fontFamily: "monospace",
+                                fontSize: "0.75rem",
+                              }}
+                              className="text-center"
+                              >
+                              {period.score}
+                            </Typography>
+                          </Box>                          
+                        ))}
+                        <Typography
+                          sx={{
+                            fontFamily: "monospace",
+                            fontSize: "1.3rem",
+                            color: game.gameStatus === 3 && game.awayTeam.score > game.homeTeam.score ? "#10833C" : "white",
+                          }}> {game.awayTeam.score} </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+
+                  <Box
+                    sx={{
+                      display: "grid",
+                      gridTemplateColumns: "80px 80px 240px 1fr",
+                      alignItems: "center",
+                    }}
+                    >
+                    <Typography variant="h6"
+                      sx={{
+                        color: game.gameStatus === 3 && game.homeTeam.score > game.awayTeam.score ? "#10833C" : "white",
+                      }}> {game.homeTeam.teamTriCode} 
+                    </Typography>
+
+                    <Typography
+                      sx={{
+                        fontWeight: "bold",
+                        fontSize: "0.75rem",
+                      }}> {game.homeTeam.wins} - {game.homeTeam.losses} 
+                    </Typography>
+
+                    <Box className="mt-2">
+                        <Box className="grid grid-cols-5 text-white gap-x-8 ml-40">
+                          {game.homeTeam.periods.map((period, index) => (
+                            <Box key={index} className="text-center">
+                              <Typography 
+                                sx={{fontFamily: "monospace"}}
+                                className="text-center"
+                                >
+                                {period.period}
+                              </Typography>
+
+                              <Typography
+                                sx={{
+                                  fontFamily: "monospace",
+                                  fontSize: "0.75rem",
+                                }}
+                                className="text-center"
+                                >
+                                {period.score}
+                              </Typography>
+                            </Box>                          
+                          ))}
+                          <Typography
+                            sx={{
+                              fontFamily: "monospace",
+                              fontSize: "1.3rem",
+                              color: game.gameStatus === 3 && game.homeTeam.score > game.awayTeam.score ? "#10833C" : "white",
+                            }}> {game.homeTeam.score} 
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </Box>
+
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "1rem",
                       fontFamily: "monospace",
-                      width: "20rem",
-                      marginLeft: "30rem",
-                      textAlign: "right",
-                    }}> {gameClockConverter(game.gameClock)} 
-                  </Typography>
-                )}
-              </Box>
-            </Box>
-          ))
+                      marginTop: "1rem",
+                      
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontSize: "1.2rem",
+                        fontWeight: "bold",
+                        fontFamily: "monospace",
+                        width: "20rem",
+                        marginLeft: "30rem",
+                        marginTop: "-12rem",
+                        textAlign: "right",
+
+                      }}> {game.gameStatusText} 
+                    </Typography>
+
+                    {game.gameStatus === 2 && (
+                      <Typography
+                        sx={{
+                          fontSize: "1.2rem",
+                          fontFamily: "monospace",
+                          width: "20rem",
+                          marginLeft: "30rem",
+                          textAlign: "right",
+                        }}> {gameClockConverter(game.gameClock)} 
+                      </Typography>
+                    )}
+                  </Box>
+                </Box>
+              ))
+            )}
+          </>
         )}
       </Box>
     </Box>
