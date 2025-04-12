3b648205923ccbcc6a7cbea1470c29cc8636e379
Kenneth Thompson <kenneththompson0220@gmail.com>
2025-04-11 22:16:26 -0500

    Moved vlrggapi, added valorantproapi and test case

---
+++ backend/app/test_upcoming.py
@@ -0,0 +1,17 @@
+from vlrggapi.api.scrape import Vlr
+import json
+from valorantproapi import data as valdata
+
+# Get VALORANT upcoming matches from API
+def main():
+    try:
+        player_kills = []
+        
+
+    except Exception as e:
+        print("Error retrieving live matches:", e)
+
+    
+
+if __name__ == "__main__":
+    main()
